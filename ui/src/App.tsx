fb1c259369a84ca0d36ee81b807ead9373158d64
timegated <beccdaniel@gmail.com>
2023-02-03 07:22:46 -0800

    moved relevant ui files to ui director

---
+++ ui/src/App.tsx
@@ -0,0 +1,12 @@
+import iconArrowDown from './assets/icon-arrow-down.svg'
+
+const App = (): JSX.Element => {
+  return (
+    <>
+      <div>Invoice App</div>
+      <img src={iconArrowDown} alt="" />
+    </>
+  )
+}
+
+export default App;

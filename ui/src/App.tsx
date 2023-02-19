df51afdfeaf6357d29b753ed6a4e0ef13f3d808e
timegated <beccdaniel@gmail.com>
2023-02-18 20:18:36 -0800

    re-installed with yarn

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,10 @@
 import iconArrowDown from './assets/icon-arrow-down.svg'
+import './app.css';
 
 const App = (): JSX.Element => {
   return (
     <>
-      <div>Invoice App</div>
+      <div className="main-content">Add Router and Navigation</div>
       <img src={iconArrowDown} alt="" />
     </>
   )

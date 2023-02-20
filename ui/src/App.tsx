e0d980b5d765bd31344861c41d19d39280510c65
timegated <beccdaniel@gmail.com>
2023-02-19 21:13:36 -0800

    Fleshing out navigation with logo and other design elements for nav-header

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,14 +1,11 @@
 import Navigation from './components/Navigation/Navigation';
-import iconArrowDown from './assets/icon-arrow-down.svg'
 import './app.css';
 
 const App = (): JSX.Element => {
   return (
-    <>
+    <section className="main-content">
       <Navigation />
-      <div className="main-content">Add Router and Navigation</div>
-      <img src={iconArrowDown} alt="" />
-    </>
+    </section>
   )
 }
 
--- ui/src/components/Navigation/Navigation.tsx
+++ ui/src/components/Navigation/Navigation.tsx
@@ -1,10 +1,21 @@
 import React, { useState } from 'react'
+import logo from '../../assets/logo.svg';
+import './navigation.css';
 
 const Navigation = () => {
   const [first, setfirst] = useState("");
-  
+
   return (
-    <div>Navigation</div>
+    <section
+    className="nav-container"
+    >
+      <div className="nav-item-header">
+        <div className="bg-color-1"></div>
+        <div className="bg-color-2"></div>
+        <img src={logo} alt="Invoice app logo" />
+      </div>
+      <div className="nav-item-footer">Nav2</div>
+    </section>
   )
 }
 

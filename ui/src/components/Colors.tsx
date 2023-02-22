96da539c98416c7efa9e783c2e2535152f6023fd
timegated <beccdaniel@gmail.com>
2023-02-21 20:34:36 -0800

    delete unused components

--- ui/src/components/Colors.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const Colors = () => {
-  return (
-    <div>Colors</div>
-  )
-};
-
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import {
   createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link
+  RouterProvider
 } from 'react-router-dom';
 
 import './main.css'
-import ColorList from './components/ColorList';
+
 
 
 const router = createBrowserRouter([
@@ -18,10 +16,15 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path: '/colors',
-    element: <ColorList />
+    path: '/invoices',
+    element: <div>Invoice Component</div>
+  },
+  {
+    path: '/invoices/:id',
+    element: <div>Invoice by ID</div>
   }
-])
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
